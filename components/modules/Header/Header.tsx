dc3ca9f6114c38d951b5d108067de16f323aa6e8
Andrey Spiney <96716371+Father1993@users.noreply.github.com>
2024-07-13 12:45:29 +1000

    Update Header.tsx

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -2,13 +2,11 @@ import HeaderTop from './HeaderTop'
 import HeaderBottom from './HeaderBottom'
 import styles from '@/styles/header/index.module.scss'
 
-const Header = () => {
-  return (
-    <header className={styles.header}>
-      <HeaderTop />
-      <HeaderBottom />
-    </header>
-  )
-}
+const Header = () => (
+  <header className={styles.header}>
+    <HeaderTop />
+    <HeaderBottom />
+  </header>
+)
 
 export default Header
